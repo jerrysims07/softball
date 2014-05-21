f6505f4c0c4eb48a47f1e417670c48e6055625ed
Jerry Sims <jerry@Jerrys-MacBook-Pro.local>
2014-05-21 15:58:31 -0500

    added data to development env

--- app/models/game.rb
+++ app/models/game.rb
@@ -3,6 +3,6 @@ class Game < ActiveRecord::Base
   belongs_to :away_team, class_name: 'Team', foreign_key: 'away_team_id'
 
   def teams
-    [home_team, away_team]
+    [home_team.name, away_team.name]
   end
 end
