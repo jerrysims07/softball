db0e48f3f05800e735550fd2b6d25b2e706fc418
Jerry Sims <jerry@Jerrys-MacBook-Pro.local>
2014-05-23 23:59:12 -0500

    now creating games with no score

--- app/controllers/games_controller.rb
+++ app/controllers/games_controller.rb
@@ -3,4 +3,24 @@ class GamesController < ApplicationController
   def index
     @games = Game.all
   end
+
+  def new
+    @game = Game.new
+    @teams = Team.all
+  end
+
+  def create
+    home_team_id = Team.find_by(name: params['game']['home_team_id']).id
+    away_team_id = Team.find_by(name: params['game']['away_team_id']).id
+    @game = Game.new(home_team_id: home_team_id, away_team_id: away_team_id)
+    if @game.save
+      redirect_to :action => 'index'
+    else
+      render :action => 'new'
+    end
+  end
+
+  def list game
+    @games = Game.all
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,7 @@ Rails.application.routes.draw do
   resources :teams
 
   get 'static_pages/home'
+  get 'games/list'
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
