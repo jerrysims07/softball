26a50b61194db5f5a2bec12744bf106b26ecb957
Jerry Sims <jerrysims07@gmail.com>
2014-06-06 09:59:15 -0500

    adding stats for individual teams

--- app/models/team.rb
+++ app/models/team.rb
@@ -21,4 +21,24 @@ class Team < ActiveRecord::Base
     self.games.nil? ? 0 : self.wins.to_f / (self.wins + self.losses)
   end
 
+  def runs_for
+    sum = 0
+    self.games.each do |g|
+      sum += self.name == g.home_team.name ? g.home_team_runs.to_i : g.away_team_runs.to_i
+    end
+    sum
+  end
+
+  def runs_allowed
+    sum = 0
+    self.games.each do |g|
+      sum += self.name == g.home_team.name ? g.away_team_runs.to_i : g.home_team_runs.to_i
+    end
+    sum
+  end
+
+  def games_played
+    wins + losses + ties
+  end
+
 end
