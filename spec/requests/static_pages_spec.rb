2ffab8a941446e20aee18dc1aef2aabe25b0ca74
Jerry Sims <jerrysims07@gmail.com>
2014-06-13 15:29:23 -0500

    remove unused testing

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -4,15 +4,6 @@ describe "Static pages" do
 
   describe "Home page" do
 
-    it "should have the content 'Sample App'" do
-      visit '/static_pages/home'
-      expect(page).to have_content('Sample App')
-    end
-
-    it "should have the right title" do
-      visit '/static_pages/home'
-      expect(page).to have_title("Home")
-    end
 
   end
 end
